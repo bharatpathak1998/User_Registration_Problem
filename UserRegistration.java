ceb2c07fc34d1d64d6d18de316f7c18b807ae3fa
bharatpathak1998 <bharatpathak099@gmail.com>
2022-09-01 07:27:21 +0530

    Added : Last Name Validater

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,4 +1,4 @@
-package UC1;
+package UC2;
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -6,23 +6,23 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static void userFirstName(String firstName) {
+    public static void userLastName(String lastName) {
 
         Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
-        Matcher matcher = pattern.matcher(firstName);
+        Matcher matcher = pattern.matcher(lastName);
 
         if (matcher.find()) {
-            System.out.println("Valid -> " + firstName);
+            System.out.println("Valid -> " + lastName);
         } else {
-            System.out.println("Invalid -> " + firstName);
+            System.out.println("Invalid -> " + lastName);
         }
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Valid First Name : ");
-        String isValidFirstName = sc.nextLine();
-        userFirstName(isValidFirstName);
+        System.out.println("Enter Valid Last Name : ");
+        String isValidLastName = sc.nextLine();
+        userLastName(isValidLastName);
     }
 }
